3d0d5802e2e9d04637a984223e246ce451861113
lathbridge <lathbridge51@gmail.com>
2023-02-09 16:30:09 +0000

    tuples

---
+++ array.py
@@ -0,0 +1,7 @@
+print("sometimes we need either a list, tuple, dictionary or a numpy imported for array to be able to use arrays in python ")
+
+# Tuples can be used like arrays in pythons
+
+tupleexample = ('samuel', 'akwasi', 'micheal')
+
+print(tupleexample[2])
